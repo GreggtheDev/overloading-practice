b4e9d93d61be41a757a8739a6fb7dd41623af98b
GreggtheDev <greggthedev@gmail.com>
2024-07-26 11:17:45 -0400

    added getters to retrieve publication year, titles and number of pages

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -22,3 +22,25 @@ public class Book {
     public Book() {
         this("Unknown", 0, 0);
     }
+
+    // Getter method to retrieve the title of the book
+    public String getTitle() {
+        return title;
+    }
+
+    // Getter method to retrieve the number of pages
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    // Getter method to retrieve the publication year
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    // Overriding toString() method to return a string representation of the Book object
+    @Override
+    public String toString() {
+        return "Book{title='" + getTitle() + "', numberOfPages=" + getNumberOfPages() + ", publicationYear=" + getPublicationYear() + "}";
+    }
+}
