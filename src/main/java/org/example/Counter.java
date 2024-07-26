b6cb1add21891b98d503582f0029b47c333afb7e
GreggtheDev <greggthedev@gmail.com>
2024-07-26 12:25:17 -0400

    added constructors to set counter initial value

--- src/main/java/org/example/Counter.java
+++ src/main/java/org/example/Counter.java
@@ -13,3 +13,39 @@ public class Counter {
     public Counter() {
         this(0);
     }
+
+    // Method that returns the current value of the counter
+    public int value() {
+        return value;
+    }
+
+    // Method that increases the value by 1
+    public void increase() {
+        value++;
+    }
+
+    // Overloaded method that increases the value by the value of increaseBy
+    public void increase(int increaseBy) {
+        if (increaseBy > 0) {
+            value += increaseBy;
+        }
+    }
+
+    // Method that decreases the value by 1
+    public void decrease() {
+        value--;
+    }
+
+    // Overloaded method that decreases the value by the value of decreaseBy
+    public void decrease(int decreaseBy) {
+        if (decreaseBy > 0) {
+            value -= decreaseBy;
+        }
+    }
+
+    // Overriding toString() method to return a string representation of the Counter object
+    @Override
+    public String toString() {
+        return "Counter{value=" + value + "}";
+    }
+}
