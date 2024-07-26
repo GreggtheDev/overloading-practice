80b8c682dcb614542cf214912322f3c07cf7a61b
GreggtheDev <greggthedev@gmail.com>
2024-07-26 10:20:41 -0400

    added objects using different constructors

--- src/main/java/org/example/MainTvShow.java
+++ src/main/java/org/example/MainTvShow.java
@@ -7,3 +7,8 @@ public class MainTvShow {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Creating a Scanner object to read user input
         ArrayList<TvShow> tvShows = new ArrayList<>(); // Creating an ArrayList to store TvShow objects
+
+        // Adding objects using different constructors
+        tvShows.add(new TvShow("Breaking Bad", 62, "Crime")); // Using constructor with all parameters
+        tvShows.add(new TvShow("Friends", "Comedy")); // Using constructor with name and genre
+        tvShows.add(new TvShow()); // Using default constructor
