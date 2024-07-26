43437c3e8b84774cfbc3c94f981d67a847688f50
GreggtheDev <greggthedev@gmail.com>
2024-07-26 12:19:15 -0400

    added constructors to set counter initial value

--- src/main/java/org/example/Counter.java
+++ src/main/java/org/example/Counter.java
@@ -3,3 +3,13 @@ package org.example;
 // Class representing a Counter
 public class Counter {
     private int value; // Attribute to store the current value of the counter
+
+    // Constructor that sets the start value of the counter to "startValue"
+    public Counter(int startValue) {
+        this.value = startValue;
+    }
+
+    // Constructor that sets the start value of the counter to 0
+    public Counter() {
+        this(0);
+    }
