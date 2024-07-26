cecc7ca47f9d838ba3f5118168f44aac80e57de9
GreggtheDev <greggthedev@gmail.com>
2024-07-26 10:54:44 -0400

    added loop to read user input

--- src/main/java/org/example/MainTvShow.java
+++ src/main/java/org/example/MainTvShow.java
@@ -12,3 +12,17 @@ public class MainTvShow {
         tvShows.add(new TvShow("Breaking Bad", 62, "Crime")); // Using constructor with all parameters
         tvShows.add(new TvShow("Friends", "Comedy")); // Using constructor with name and genre
         tvShows.add(new TvShow()); // Using default constructor
+
+        // Loop to read user input
+        while (true) {
+            System.out.print("Enter TV show name: ");
+            String showName = scanner.nextLine(); // Reading the TV show name from user input
+            if (showName.isEmpty()) {
+                break; // If the input is empty, break the loop
+            }
+            System.out.print("Enter number of episodes: ");
+            int numberOfEpisodes = Integer.parseInt(scanner.nextLine()); // Reading the number of episodes from user input
+            System.out.print("Enter genre: ");
+            String genre = scanner.nextLine(); // Reading the genre from user input
+            tvShows.add(new TvShow(showName, numberOfEpisodes, genre)); // Adding a new TvShow object to the list
+        }
