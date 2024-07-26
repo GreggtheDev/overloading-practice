914d91faf9eeab3b3a1b1eb291c2db50f913481b
GreggtheDev <greggthedev@gmail.com>
2024-07-25 21:29:37 -0400

    created tvshow class

---
+++ src/main/java/org/example/TvShow.java
@@ -0,0 +1,7 @@
+package org.example;
+
+// Class representing a TV Show
+public class TvShow {
+    private String showName; // Attribute to store the name of the TV show
+    private int numberOfEpisodes; // Attribute to store the number of episodes
+    private String genre; // Attribute to store the genre of the TV show
