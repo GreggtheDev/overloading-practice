672d6f13c6c96c2a4f5400bc174bc5a640ea2165
GreggtheDev <greggthedev@gmail.com>
2024-07-26 10:16:05 -0400

    created class MainTvShow

---
+++ src/main/java/org/example/MainTvShow.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.ArrayList; // Importing ArrayList to store TvShow objects
+import java.util.Scanner; // Importing Scanner to read user input
+
+public class MainTvShow {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Creating a Scanner object to read user input
+        ArrayList<TvShow> tvShows = new ArrayList<>(); // Creating an ArrayList to store TvShow objects
