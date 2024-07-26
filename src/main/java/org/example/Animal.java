a65c0a79c50922f9d9f46b3a30e8096197019671
GreggtheDev <greggthedev@gmail.com>
2024-07-25 20:59:49 -0400

    created Animal class

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,6 @@
+package org.example;
+
+// Class representing an Animal
+public class Animal {
+    private String name; // Attribute to store the name of the animal
+    private boolean isDog; // Attribute to store if the animal is a dog or not
