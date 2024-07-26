0e2f1bb643885cfae9e66fcc1228fa33672a4edc
GreggtheDev <greggthedev@gmail.com>
2024-07-25 21:13:30 -0400

    created MainAnimal class

---
+++ src/main/java/org/example/MainAnimal.java
@@ -0,0 +1,7 @@
+package org.example; // Ensure the package matches your directory structure
+
+import java.util.ArrayList; // Importing ArrayList to store Animal objects
+import java.util.Scanner; // Importing Scanner to read user input
+
+public class MainAnimal {
+}
