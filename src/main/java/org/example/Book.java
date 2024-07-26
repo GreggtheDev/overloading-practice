12a86bd713e5773329e3227808aebc29358e6757
GreggtheDev <greggthedev@gmail.com>
2024-07-26 11:02:16 -0400

    created book class

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,7 @@
+package org.example;
+
+// Class representing a Book
+public class Book {
+    private String title; // Attribute to store the title of the book
+    private int numberOfPages; // Attribute to store the number of pages
+    private int publicationYear; // Attribute to store the publication year
